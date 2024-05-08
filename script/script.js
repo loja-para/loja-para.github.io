ba543d0a04a933d55f173e93637ca6ffcdb4c16a
Matheus Martins <106982245+mathmar-tins@users.noreply.github.com>
2024-05-08 23:08:40 +0000

    feat: Adicionar funcionalidade de carrinho de compras

--- script/script.js
+++ script/script.js
@@ -5,9 +5,45 @@ function openPanel() {
 function closePanel() {
     document.getElementById('sidePanel').style.right = '-300px';
 }
+// Crie um array para armazenar os itens do carrinho
 var carrinho = [];
-function adicionarGarrafa(){
-    var produto = document.getElementById("cheiro").value;
-    carrinho.push(produto);
-    alert("Produto adicionado ao carrinho!");
+
+// Função para adicionar item ao carrinho
+function adicionarGarrafa(nome, preco, imagemUrl) {
+    // Adicione o item ao array carrinho
+    carrinho.push({
+        nome: 'Garrafinhas de Cheiro',
+        preco: 3.50,
+        imagemUrl: 'imagens/conteudo/garrafasCheiro.jpg'
+    });
+
+    // Atualize o painel do carrinho
+    atualizarCarrinho();
+}
+
+// Função para remover item do carrinho
+function removerItem(index) {
+    // Remova o item do array carrinho
+    carrinho.splice(index, 1);
+
+    // Atualize o painel do carrinho
+    atualizarCarrinho();
+}
+
+// Função para atualizar o painel do carrinho
+function atualizarCarrinho() {
+    // Obtenha o elemento do contêiner de itens do carrinho
+    var containerItens = document.getElementById('itensCarrinho');
+
+    // Limpe o contêiner de itens
+    containerItens.innerHTML = '';
+
+    // Adicione cada item do carrinho ao contêiner
+    for (var i = 0; i < carrinho.length; i++) {
+        var item = carrinho[i];
+        containerItens.innerHTML += '<img src="' + item.imagemUrl + '" alt="' + item.nome + '" style="width: 115px; height: 75px;"><p>' + item.nome + ' - R$ ' + item.preco.toFixed(2) + '</p><button onclick="removerItem(' + i + ')">Remover</button>';
+    }
+}
+function fecharCarrinho() {
+    document.getElementById('sidePanel').style.right = '-300px';
 }
