69e517b09c3f40b40940e5361377fb06bf39921b
H-ricky-w <82538790+H-ricky-w@users.noreply.github.com>
2024-06-10 23:10:21 +0000

    refactor: Update JavaScript script reference to script.js

--- script/script.js
+++ script/script.js
@@ -1,15 +1,164 @@
 var quantidade = 0;
 var total = 0;
-var produtos = [
+
+// Crie um array para armazenar os itens do carrinho
+var produtos = JSON.parse(localStorage.getItem('produtos')) || [0,0,0,0,0,0];
+
+// Função para adicionar item ao carrinho
+function adicionarCarrinho(item)
+{
+    
+    produtos[item]++;
+    //mostra o item no carrinho
+    alterarQuantidade(produtos[item]);
+    salvarCarrinho();
+    recuperarCarrinho();
+}
+
+// Função para salvar o carrinho no localStorage
+function salvarCarrinho() {
+    localStorage.setItem('produtos', JSON.stringify(produtos));
+}
+
+// Função para recuperar o carrinho do localStorage
+function recuperarCarrinho()
+{
+    var produtosRecuperados = localStorage.getItem('produtos');
+    if (produtosRecuperados)
     {
-        garrafinhas: 0,
-        jambu: 0,
-        barco: 0,
-        biojoia: 0,
-        cuia: 0,
-        vaso: 0
+        produtos = JSON.parse(produtosRecuperados);
+
+        total = 0;
+
+        for (var i = 0; i < produtos.length; i++)
+        {
+            var quantidadeProduto = produtos[i];
+            var precoProduto = parseFloat(document.getElementById('preco' + i).textContent);
+            
+            total += quantidadeProduto * precoProduto;
+            quantidade += quantidadeProduto;
+
+            exibirProduto(i);
+            alterarQuantidade(produtos[i]);
+        }
+
+        document.getElementById('total').textContent = 'R$ ' + total.toFixed(2);
+
+        if ( quantidade > 0 )
+        {
+            document.getElementById('botaoPagar').style.display = 'block';
+        }
+        else
+        {
+            document.getElementById('botaoPagar').style.display = 'none';
+        }
     }
-];
+}
+
+// Placeholder para a função alterarQuantidade
+function alterarQuantidade(item)
+{
+    if ( item == 0 )
+    {
+        document.getElementById("quantidadeGarrafas").textContent = produtos[item]
+    }
+    else if (item == 1 )
+    {
+        document.getElementById("quantidadeJambu").textContent = produtos[item]
+    }
+    else if ( item == 2 )
+    {
+        document.getElementById("quantidadeBarcos").textContent = produtos[item]
+    }
+    else if ( item == 3 )
+    {
+        document.getElementById("quantidadeBiojoias").textContent = produtos[item]
+    }
+    else if ( item == 4 )
+    {
+        document.getElementById("quantidadeCuias").textContent = produtos[item]
+    }
+    else if ( item == 5 )
+    {
+        document.getElementById("quantidadeVasos").textContent = produtos[item]
+    }
+}
+
+
+function exibirProduto(i)
+{
+    switch (i)
+    {
+        case 0:
+            if(produtos[i] > 0)
+            {
+                document.getElementById('garrafinhas').style.display = 'block';
+                break;
+            }
+            else if(produtos[i] == 0)
+            {
+                document.getElementById('garrafinhas').style.display = 'none';
+                break;
+            }
+        case 1:
+            if(produtos[i] > 0)
+            {
+                document.getElementById('jambu').style.display = 'block';
+                break;
+            }
+            else if(produtos[i] == 0)
+            {
+                document.getElementById('jambu').style.display = 'none';
+                break;
+            }
+        case 2:
+            if(produtos[i] > 0)
+            {
+                document.getElementById('barco').style.display = 'block';
+                break;
+            }
+            else if(produtos[i] == 0)
+            {
+                document.getElementById('barco').style.display = 'none';
+                break;
+            }
+        case 3:
+            if(produtos[i] > 0)
+            {
+                document.getElementById('biojoia').style.display = 'block';
+                break;
+            }
+            else if(produtos[i] == 0)
+            {
+                document.getElementById('biojoia').style.display = 'none';
+                break;
+            }
+        case 4:
+            if(produtos[i] > 0)
+            {
+                document.getElementById('cuia').style.display = 'block';
+                break;
+            }
+            else if(produtos[i] == 0)
+            {
+                document.getElementById('cuia').style.display = 'none';
+                break;
+            }
+        case 5:
+            if(produtos[i] > 0)
+            {
+                document.getElementById('vaso').style.display = 'block';
+                break;
+            }
+            else if(produtos[i] == 0)
+            {
+                document.getElementById('vaso').style.display = 'none';
+                break;
+            }
+    }
+}
+
+
 
 function showNotification()
 {
@@ -25,6 +174,10 @@ function showNotification()
 function openPanel()
 {
     document.getElementById('sidePanel').style.right = '0';
+
+    
+
+
     if ( quantidade == 0 )
     {
         document.getElementById('botaoPagar').style.display = 'none';
@@ -40,89 +193,16 @@ function closePanel()
     document.getElementById('sidePanel').style.right = '-400px';
 }
 
-// Crie um array para armazenar os itens do carrinho
-
-
-
-// Função para adicionar item ao carrinho
-function adicionarCarrinho(item, preco)
-{
-    preco = parseFloat(preco);
-    total += preco;
-
-    produtos[0][item] += 1;
-    quantidade += 1;
-    //mostra o item no carrinho
-
-    var mostrar = document.getElementById(item).style.display;
-
-    if ( mostrar == 'none' || mostrar == '' )
-    {
-        document.getElementById(item).style.display = 'block';
-        document.getElementById('botaoPagar').style.display = 'block';
-    }
-
-    document.getElementById('total').textContent = 'R$ ' + total.toFixed(2);
-
-    alterarQuantidade();
-}
 
 // Função para remover item do carrinho
-function remover(item, preco)
+function remover(item)
 {
-
-    preco = parseFloat(preco);
-    total -= preco;
-
-    produtos[0][item] -= 1;
-    quantidade -= 1;
-
-    if ( quantidade <= 0 )
-    {
-        quantidade = 0;
-    }
-    
-    //mostra o item no carrinho
-    if ( produtos[0][item] < 1 )
-    {    
-        var mostrar = document.getElementById(item).style.display;
-        if ( mostrar == 'block' )
-        {
-            document.getElementById(item).style.display = 'none';
-        }
-        if ( quantidade == 0 )
-        {
-            document.getElementById('botaoPagar').style.display = 'none';
-        }
-    }
-
-    document.getElementById('total').textContent = 'R$ ' + total.toFixed(2);
+    produtos[item]--;
+    salvarCarrinho();
+    recuperarCarrinho();
 }
 
-alterarQuantidade()
-{
-    document.getElementById('quantidadeGarrafas').textContent = produtos[0][0];
-    document.getElementById('quantidadeJambu').textContent = produtos[0][1];
-    document.getElementById('quantidadeBarcos').textContent = produtos[0][2];
-    document.getElementById('quantidadeBiojoias').textContent = produtos[0][3];
-    document.getElementById('quantidadeCuias').textContent = produtos[0][4];
-    document.getElementById('quantidadeVasos').textContent = produtos[0][5];
-}
-
+// Event listener para carregar a janela e recuperar o carrinho
 window.onload = function() {
-    // Recupere o carrinho do localStorage
-    const carrinhoSalvo = JSON.parse(localStorage.getItem('carrinho'));
-
-    if (carrinhoSalvo) {
-        carrinho = carrinhoSalvo;
-
-        // Mostra os itens no carrinho
-        for (let item of carrinho) {
-            document.getElementById(item).style.display = 'block';
-        }
-
-        if (carrinho.length > 0) {
-            document.getElementById('botaoPagar').style.display = 'block';
-        }
-    }
+    recuperarCarrinho();
 }
